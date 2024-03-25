980e15287667d0982a3e88208af0a4918b5bcd99
Deivid da Silva G <92928385+IF-DeividSilva@users.noreply.github.com>
2024-03-25 17:20:36 -0300

    Update Main.java

--- AP1-parcial/src/Main.java
+++ AP1-parcial/src/Main.java
@@ -8,35 +8,34 @@
  * 
  * @copyright Copyright (c) 2022
  * 
-
  */
-
 import java.util.LinkedList;
 import java.util.List;
-
+// implementar o <EOF>
 //java Main programa1.gyh
 
 public class Main {
     public static void main(String[] args) {
        // if (args.length > 0) {
             String nomeArq = "programa2.gyh";//args[0];
-            
-            PadronizaArquivo.padroniza(nomeArq);//
-            
+            // chama o metodo da class padroniza para add um espaço antes e dps dos delimitadores
+            PadronizaArquivo.padroniza(nomeArq);
+            // salva as linhas do arq ja padronizado em um vetor de strings
             List<String> linhas = LerArquivo.lerArquivo(nomeArq);
+            // cria uma lista encadeada de tokens (no fim foi inutil)
             LinkedList<Token> tokens = new LinkedList<Token>();
+            // variavel para saber se está na parte do codigo de declaração("dec") ou na parte de "prog"
             int controle = 0;
-
+            // var para o indice da linha
             int indice = 0;
+            // for para enquanto tiver linhas ele chamar o metodo analisador da classe analizador lexico 
             for (String linha : linhas) {
                 indice++;
                 controle = AnalisadorLexico.analisador (linha, indice, tokens, controle);
             }
+            // for para colocar os tokens em um arquivo de saida
             for (Token token : tokens) {
                 System.out.println(token);
             }
-        //} else {
-          //  System.out.println("Por favor, forneça o caminho do arquivo como argumento.");
-        //}
     }
 }
