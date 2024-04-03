d7cf51d955f9254574f68759cb716fc629743592
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2024-04-02 22:10:58 -0300

    commit

--- Compiladores_OF/AP1-parcial/src/ValidaVar.java
+++
@@ -1,8 +0,0 @@
-public class ValidaVar {
-    public static boolean validaVar(String var) {
-        if (var.matches("[A-Z][a-zA-Z0-9]*")) {
-            return true;
-        }
-        return false;
-    }
-}
